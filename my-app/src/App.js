48605e1af192002ea552214382e6315e0c76194b
HubRob <nejbert@gmx.de>
2018-12-05 16:51:36 +0100

    Eigene Branch neues Icon und Textanpassungen
    Für die Anpassungen auf der Startseite habe ich einen neuen Branch erzeugt und einige Änderungen am Icon sowie am Text vorgenommen.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './book.svg';
 import './App.css';
 
 class App extends Component {
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+          	Das war nun mein erster Commit.
           </p>
           <a
             className="App-link"
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+  
           </a>
         </header>
       </div>
